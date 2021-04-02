0cb9cdece26b5325254b8dd559f9070d6beab6cd
braggs <emielvdbrink@gmail.com>
2021-04-02 13:17:06 +0200

    added functions for joining subgraphs together

--- VertexCover/VertexCover/MainWindow.xaml.cs
+++ VertexCover/VertexCover/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Linq;
 
 namespace VertexCover
 {
@@ -30,7 +31,8 @@ namespace VertexCover
             GenerateWindow generateWindow = new GenerateWindow();
             generateWindow.ShowDialog();
 
-            GenerateAdjacencyMatrix(generateWindow.Nodes, generateWindow.Density);
+            bool[,] matrix = GenerateAdjacencyMatrix(generateWindow.Nodes, generateWindow.Density);
+            List<List<int>> subGraphs = FindSubGraphsFromAdjacencyMatrix(matrix);
         }
 
         private bool[,] GenerateAdjacencyMatrix(int vertices, int edgeProbability)
@@ -42,11 +44,56 @@ namespace VertexCover
             {
                 for(int j = 0; j < vertices; j++)
                 {
+                    if(i == j)
+                    {
+                        break;
+                    }
                     matrix[i, j] = random.Next(0, 101) <= edgeProbability;
+                    matrix[j, i] = matrix[i, j];
                 }
             }
 
             return matrix;
+        }
+
+        private List<List<int>> FindSubGraphsFromAdjacencyMatrix(bool[,] adjacencyMatrix)
+        {
+            List<List<int>> subGraphs = new List<List<int>>();
+            List<int> nodes = new List<int>(Enumerable.Range(0, adjacencyMatrix.GetLength(0)));
+
+            for(int i = 0; i < nodes.Count; i++)
+            {
+                int currentNode = nodes[i];
+                List<int> currentSubGraph = new List<int>();
+                currentSubGraph.Add(currentNode);
+
+                for(int j = 0; j < currentSubGraph.Count; j++)
+                {
+                    currentNode = currentSubGraph[j];
+                    for (int k = 0; k < adjacencyMatrix.GetLength(1); k++)
+                    {
+                        if (adjacencyMatrix[currentNode, k] && !currentSubGraph.Contains(k))
+                        {
+                            currentSubGraph.Add(k);
+                            nodes.Remove(k);
+                        }
+                    }
+                }
+                subGraphs.Add(currentSubGraph);
+            }
+
+            return subGraphs;
+        }
+
+        private bool[,] ConnectSubGraphsInAdjacencyMatrix(bool[,] adjacencyMatrix, int[][] subGraphs)
+        {
+            for(int i = 0; i < subGraphs.Length - 1; i++)
+            {
+                adjacencyMatrix[subGraphs[i][0],subGraphs[i+1][0]] = true;
+                adjacencyMatrix[subGraphs[i + 1][0], subGraphs[i][0]] = true;
+            }
+
+            return adjacencyMatrix;
         }
     }
 }
