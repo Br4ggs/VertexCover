d7b8e5075018919a7d251debb1cfe51ed2db1f80
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2021-04-13 23:02:19 +0200

    Fix parse exception

--- VertexCover/VertexCover/GenerateVertexCoverWindow.xaml.cs
+++ VertexCover/VertexCover/GenerateVertexCoverWindow.xaml.cs
@@ -41,7 +41,8 @@ namespace VertexCover
 
         private void NodesTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Nodes = Int32.Parse((sender as TextBox).Text);
+            if (int.TryParse((sender as TextBox)?.Text, out var value))
+                Nodes = value;
         }
 
         private void ConfirmButton_Click(object sender, RoutedEventArgs e)
