dbd66cc7c7dea692c0aa224bea29cfc1bde1c547
braggs <emielvdbrink@gmail.com>
2021-04-19 10:55:18 +0200

    added stump of graphkernelizer class

---
+++ VertexCover/VertexCover/Src/GraphKernelizer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace VertexCover
+{
+    public class GraphKernelizer
+    {
+        private Random random = new Random();
+
+        public KernelizedAttributes FindKernelizedAttributes(Graph graph, int k)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Vertex> FindPendantVertices(Graph graph)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Vertex> FindTopVertices(Graph graph, int k)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyCollection<Vertex> FindIsolatedVertices(Graph graph)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public struct KernelizedAttributes
+    {
+        IReadOnlyCollection<Vertex> Pendants { get; }
+        IReadOnlyCollection<Vertex> Tops { get; }
+        IReadOnlyCollection<Vertex> Independents { get; }
+        Graph Graph { get; }
+
+        public KernelizedAttributes(
+            IReadOnlyCollection<Vertex> pendants, 
+            IReadOnlyCollection<Vertex> tops,
+            IReadOnlyCollection<Vertex> independents,
+            Graph graph)
+        {
+            Pendants = pendants;
+            Tops = tops;
+            Independents = independents;
+            Graph = graph;
+        }
+    }
+}
