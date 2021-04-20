c31cfd45e42dcf780d891ab3659a8f70f2ab22b5
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2021-04-20 20:38:30 +0200

    Add tests for graphs

---
+++ VertexCover/VertexCoverTests/Src/Graph/EdgeTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VertexCover;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VertexCover.Tests
+{
+    [TestClass()]
+    public class EdgeTests
+    {
+        [TestMethod()]
+        public void EdgeTest()
+        {
+            Vertex startVertex = new Vertex(0);
+            Vertex endVertex = new Vertex(1);
+
+            Edge edge = new Edge(startVertex, endVertex);
+            Assert.AreEqual(edge.StartVertex, startVertex);
+            Assert.AreEqual(edge.EndVertex, endVertex);
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Vertex startVertex = new Vertex(0);
+            Vertex endVertex = new Vertex(1);
+            Edge edge = new Edge(startVertex, endVertex);
+            Edge edge2 = new Edge(startVertex, endVertex);
+            Assert.IsTrue(Equals(edge, edge2));
+        }
+
+        [TestMethod()]
+        public void EqualsTest_NotEqual()
+        {
+            Vertex startVertex = new Vertex(0);
+            Vertex endVertex = new Vertex(1);
+            Edge edge = new Edge(startVertex, endVertex);
+            Edge edge2 = new Edge(startVertex, new Vertex(2));
+            Assert.IsFalse(Equals(edge, edge2));
+        }
+
+        [TestMethod()]
+        public void EqualsTest_ReverseStartAndEndVertex()
+        {
+            Vertex startVertex = new Vertex(0);
+            Vertex endVertex = new Vertex(1);
+            Edge edge = new Edge(startVertex, endVertex);
+            Edge edge2 = new Edge(endVertex, startVertex);
+            Assert.IsFalse(Equals(edge, edge2));
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            Vertex startVertex = new Vertex(0);
+            Vertex endVertex = new Vertex(1);
+            Edge edge = new Edge(startVertex, endVertex);
+            Assert.AreEqual(HashCode.Combine(startVertex, endVertex), edge.GetHashCode());
+        }
+    }
+}
