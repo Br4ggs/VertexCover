7f62dd4f0db99c3c27290ae8bd50aad8c93ae821
braggs <emielvdbrink@gmail.com>
2021-04-21 13:15:42 +0200

    added preprocessor and unit tests

---
+++ VertexCover/VertexCover/Src/GraphPreprocessor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VertexCover
+{
+    //todo:
+    //-turn GraphKernelizer into an instance class
+    public class GraphPreprocessor
+    {
+        public PreProcessedGraphAttributes GetVertexCoverProcessedGraph(Graph graph)
+        {
+            Graph preprocessedGraph = new Graph(graph);
+            List<Vertex> includedVertices = new List<Vertex>();
+            IEnumerable<Vertex> pendants = GraphKernelizer.FindPendantVertices(graph);
+            IEnumerable<Vertex> independents = GraphKernelizer.FindIsolatedVertices(graph);
+            foreach (Vertex pendent in pendants)
+            {
+                IEnumerable<Edge> connectedEdges = graph.GetEdges(pendent);
+                includedVertices.AddRange(connectedEdges.Select(edge => pendent.Equals(edge.StartVertex) ? edge.EndVertex : edge.StartVertex));
+            }
+
+            //get connected edges of included vertices
+            //remove edges
+            //remove pendents
+            //remove included vertices
+
+            //possibly, also add top vertices to included vertices
+            //possibly, remove top vertices from graph
+
+            PreProcessedGraphAttributes preProcessedGraphAttributes = new PreProcessedGraphAttributes(includedVertices, preprocessedGraph);
+            return preProcessedGraphAttributes;
+        }
+    }
+
+    public struct PreProcessedGraphAttributes
+    {
+        public IEnumerable<Vertex> IncludedVertices { get; }
+        public Graph ProcessedGraph { get; }
+
+        public PreProcessedGraphAttributes(
+            IEnumerable<Vertex> includedVertices,
+            Graph processedGraph)
+        {
+            IncludedVertices = includedVertices;
+            ProcessedGraph = processedGraph;
+        }
+    }
+}
